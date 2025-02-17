e7091e6512cd1d427a63d8c45608a6630c77549c
MariaGabrielaReis <mariagabrielagreis@gmail.com>
2025-02-17 18:24:16 -0300

    feat: create wallets resource and implements simple CRUD

---
+++ src/wallets/wallets.controller.ts
@@ -0,0 +1,35 @@
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { CreateWalletDto } from './dto/create-wallet.dto';
+import { WalletsService } from './wallets.service';
+
+@Controller('wallets')
+export class WalletsController {
+  constructor(private readonly walletsService: WalletsService) {}
+
+  @Post()
+  create(@Body() createWalletDto: CreateWalletDto) {
+    return this.walletsService.create(createWalletDto);
+  }
+
+  @Post(':id/assets')
+  createWalletAsset(
+    @Param('id') id: string,
+    @Body() body: { assetId: string; shares: number },
+  ) {
+    return this.walletsService.createWalletAsset({
+      walletId: id,
+      assetId: body.assetId,
+      shares: body.shares,
+    });
+  }
+
+  @Get()
+  findAll() {
+    return this.walletsService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.walletsService.findOne(id);
+  }
+}
---
+++ src/wallets/wallets.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { CreateWalletDto } from './dto/create-wallet.dto';
+import { WalletAsset } from './entities/wallet-asset.entity';
+import { Wallet } from './entities/wallet.entity';
+
+@Injectable()
+export class WalletsService {
+  constructor(
+    @InjectModel(Wallet.name) private walletSchema: Model<Wallet>,
+    @InjectModel(WalletAsset.name)
+    private walletAssetSchema: Model<WalletAsset>,
+  ) {}
+
+  create(createWalletDto: CreateWalletDto) {
+    return this.walletSchema.create(createWalletDto);
+  }
+
+  createWalletAsset(data: {
+    walletId: string;
+    assetId: string;
+    shares: number;
+  }) {
+    return this.walletAssetSchema.create({
+      wallet: data.walletId,
+      asset: data.assetId,
+      shares: data.shares,
+    });
+  }
+
+  findAll() {
+    return this.walletSchema.find();
+  }
+
+  findOne(id: string) {
+    return this.walletSchema.findById(id);
+  }
+}
